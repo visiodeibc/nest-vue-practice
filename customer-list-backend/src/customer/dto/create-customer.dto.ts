a45e4a2f98e312de6f4ace9c87f8ac3d93ef0f5a
Jake <jake@enuma.com>
2021-10-22 18:30:38 +0900

    first

---
+++ customer-list-backend/src/customer/dto/create-customer.dto.ts
@@ -0,0 +1,10 @@
+// ./src/customer/dto/create-customer.dto.ts
+export class CreateCustomerDTO {
+    readonly first_name: string;
+    readonly last_name: string;
+    readonly email: string;
+    readonly phone: string;
+    readonly address: string;
+    readonly description: string;
+    readonly created_at: Date;
+}
---
+++ customer-list-backend/src/customer/interfaces/customer.interface.ts
@@ -0,0 +1,12 @@
+// ./src/customer/interfaces/customer.interface.ts
+import { Document } from 'mongoose';
+
+export interface Customer extends Document {
+    readonly first_name: string;
+    readonly last_name: string;
+    readonly email: string;
+    readonly phone: string;
+    readonly address: string;
+    readonly description: string;
+    readonly created_at: Date;
+}
